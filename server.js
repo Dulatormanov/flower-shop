869ba784fe6712aff09cf0a7c0bb687e88c16f00
Dulat Ormanov <dulatpingood@gmail.com>
2025-05-23 00:41:59 +0500

    upd

--- server.js
+++ server.js
@@ -1,57 +1,24 @@
 const express = require('express');
-const { Pool } = require('pg');
-const path = require('path'); // Добавь эту строку
+const path = require('path');
+const { flowersData, bouquetsData } = require('./data.js');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Настройки подключения к базе данных
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgresql://postgres.inqxwptbhulianfgvpmo:dulatormanov2OO4@aws-0-eu-north-1.pooler.supabase.com:6543/postgres'
-});
-app.use(express.static(path.join(__dirname, 'public')));
-console.log('Static directory:', path.join(__dirname, 'public'));
-
-// Add this to check routes
-app.get('/', (req, res) => {
-  console.log('Root route accessed');
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 app.use(express.static('public'));
 
-// Обработчик для корневого пути ('/')
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Маршрут для получения данных о цветах
-app.get('/flowers', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT id, name, color, price, image_url FROM flowers');
-        const flowers = result.rows;
-        client.release();
-        res.json(flowers);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Ошибка при получении данных о цветах' });
-    }
+app.get('/flowers', (req, res) => {
+    res.json(flowersData);
 });
 
-// Маршрут для получения данных о букетах
-app.get('/bouquets', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT id, name, description, image_url, price FROM bouquets');
-        const bouquets = result.rows;
-        client.release();
-        res.json(bouquets);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Ошибка при получении данных о букетах' });
-    }
+app.get('/bouquets', (req, res) => {
+    res.json(bouquetsData);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running on port ${process.env.PORT || 3000}`);
-  });
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
