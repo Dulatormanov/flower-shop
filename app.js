28dd3a2b5c33975f2919ad3318050c3368705feb
Dulat Ormanov <dulatpingood@gmail.com>
2025-05-22 22:18:12 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { Pool } = require('pg');
+const path = require('path'); // Подключаем модуль path
+
+const app = express();
+const port = 3000;
+
+// Замените на свои параметры подключения к базе данных
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'flower_shop1',
+    password: 'Madina.2007.mama',
+    port: 5432,
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Явно указываем путь к views
+
+app.use(express.static(path.join(__dirname, 'public'))); // Передаем 'public'
+
+app.get('/', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM products');
+        const products = result.rows;
+        client.release();
+
+        res.render('index', { products: products });
+    } catch (err) {
+        console.error('Ошибка при запросе к базе данных:', err);
+        res.status(500).send('Ошибка сервера: ' + err.message); // Добавлено сообщение об ошибке
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);});
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { Pool } = require('pg');
+const path = require('path'); // Добавь эту строку
+
+const app = express();
+const port = 3000;
+
+// Настройки подключения к базе данных
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'desso-flowers',
+    password: 'Madina.2007.mama',
+    port: 5432,
+});
+
+app.use(express.static('public'));
+
+// Обработчик для корневого пути ('/')
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Маршрут для получения данных о цветах
+app.get('/flowers', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT id, name, color, price, image_url FROM flowers');
+        const flowers = result.rows;
+        client.release();
+        res.json(flowers);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Ошибка при получении данных о цветах' });
+    }
+});
+
+// Маршрут для получения данных о букетах
+app.get('/bouquets', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT id, name, description, image_url, price FROM bouquets');
+        const bouquets = result.rows;
+        client.release();
+        res.json(bouquets);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Ошибка при получении данных о букетах' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
